d8e4ffb7698ffe51c8fae8d8fe8ecd0f78c32956
Osmar <osmar.mas@correo.uady.mx>
2017-12-10 13:03:57 -0600

    Pequenios cambios a site/index

--- views/site/index.php
+++ views/site/index.php
@@ -8,24 +8,12 @@ use yii\widgets\Pjax;
 $this->title = 'SSCCE';
 ?>
 <div class="site-index">
-
     <div class="jumbotron">
         <h1>Bienvenido al Sistema de Solicitud de Constancias</h1>
         <br><br>
-		<div class= "col-lg-12" > 
-		
-		
-            <?= Html::a(Yii::t('app', 'Entrar'), ['/student/login'], ['class' => 'btn btn-success']) ?>
-       
-		
-		        
-            <?= Html::a(Yii::t('app', 'Registrarte'), ['/site/register'], ['class' => 'btn btn-primary']) ?>
-      
-		
-		</div>
-       
+    		<div class= "col-lg-12" >
+           <?= Html::a(Yii::t('app', 'Entrar'), ['/student/login'], ['class' => 'btn btn-success']) ?>
+           <?= Html::a(Yii::t('app', 'Registrarte'), ['/site/register'], ['class' => 'btn btn-primary']) ?>
+    		</div>
     </div>
-
 </div>
- 
-
